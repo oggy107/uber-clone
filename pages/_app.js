c2f2af796b604f3062e9fd0cb0a024a1ccd2f859
Oggy107 <urmalsingh10@gmail.com>
2021-11-05 17:56:12 +0530

    Added: map, body

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../styles/main.css'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/assets/files/actionBtns.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: 'Ride',
+        img: 'https://i.ibb.co/cyvcpfF/uberx.png'
+    },
+    {
+        title: '2-Wheels',
+        img: 'https://i.ibb.co/n776JLm/bike.png'
+    },
+    {
+        title: 'Reserve',
+        img: 'https://i.ibb.co/5RjchBg/uberschedule.png'
+    },
+]
