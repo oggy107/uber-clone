d7aed2c5d7572feaefc3b33a1158a4d598eb7eda
Oggy107 <urmalsingh10@gmail.com>
2021-11-13 14:21:12 +0530

    Added: back btn in comfirm.js

--- components/confirm/ConfirmBody.js
+++ components/confirm/ConfirmBody.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 
 import rides from '../assets/files/carList'
 
 const ConfirmBody = () => {
     return (
         <div className="confirmBody">
+            <Link href="/search">
+                <div className="confirmBackBtnContainer">
+                    <button className="btn confirmBackBtn">
+                        <img src="https://img.icons8.com/ios-filled/50/000000/left.png" alt="back"/>
+                    </button>
+                </div>
+            </Link>
             <Header />
             <RideSelection />
             <button className="confirmConfirmBtn btn">Confirm UberX</button>
